357e5a916138953849e5a6a93a45e870b61f61f6
Tom Zellner <tom.zellner@gmail.com>
2015-05-30 14:47:46 -0700

    finished basic home page and basic trip index

--- Hitch-Coupled/app/controllers/trips_controller.rb
+++ Hitch-Coupled/app/controllers/trips_controller.rb
@@ -28,8 +28,13 @@ class TripsController < ApplicationController
 
 
 	def index
-		trips = Trip.all
-		render json: {trips: trips}
+		@trips = Trip.all
+		respond_to do |format|
+      format.html
+      format.json { render :json => @trips }
+    end
+
+		
 
 	end
 
