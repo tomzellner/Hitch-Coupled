0f05279e618b528e3ef8d2288e1289ca760c3b3a
shriyanguna & RAAAWWRRR I'm badman <pair+shriyanguna.yon123@devbootcamp.com>
2015-05-30 14:58:33 -0700

    updated migration for user to add profile pic column

--- Hitch-Coupled/app/controllers/application_controller.rb
+++ Hitch-Coupled/app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ roles: [] }, :email, :password, :password_confirmation, :first_name, :last_name, :phonenumber, :birthdate) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ roles: [] }, :email, :password, :password_confirmation, :first_name, :last_name, :phonenumber, :birthdate, :profile_pic) }
   end# Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
