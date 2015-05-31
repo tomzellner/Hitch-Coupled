c65a7adcb1c98ae4c5dcc9bb388fe677624ed521
Tom Zellner <tom.zellner@gmail.com>
2015-05-31 15:02:08 -0700

    small fix for avg helper method

--- Hitch-Coupled/app/helpers/users_helper.rb
+++ Hitch-Coupled/app/helpers/users_helper.rb
@@ -1,11 +1,11 @@
 module UsersHelper
 	def rated_avg
-		all_array = User.find(params[:id]).rated_ratings
-			num_array=[]
-		all_array.each do |rating|
+		object_array = User.find(params[:id]).rated_ratings
+			num_array = []
+		object_array.each do |rating|
 			num_array << rating.score
 		end
-		@avg = num_array.inject{ |sum, n| sum + n }.to_f / all_array.size
+		@avg = num_array.inject{ |sum, n| sum + n }.to_f / object_array.size
 
 		return @avg
 	end
