31bd643b86dd1ea1e5ceba8735a1146e10546cd7
shriyanguna & RAAAWWRRR I'm badman <pair+shriyanguna.yon123@devbootcamp.com>
2015-05-30 17:03:17 -0700

    added user show page layout

--- Hitch-Coupled/app/controllers/users_controller.rb
+++ Hitch-Coupled/app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
 	def show
-		user = User.find(params[:id])
-		if user
-			render json: {user: user}
-		# else
-		# 	status 400
+		@user = User.find(params[:id])
+		respond_to do |format|
+			format.html
+			format.json {render :json => @user}
 		end
 	end
 
