ef25cd81ea3ed3b437937becf7a426d25cf3be16
Shriyan Gunasekera <shriyan.gunasekera@gmail.com>
2015-05-31 20:17:07 -0700

    added button to add person to trip through chat. Need to update logic

--- Hitch-Coupled/db/schema.rb
+++ Hitch-Coupled/db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150531192334) do
+ActiveRecord::Schema.define(version: 20150531213438) do
 
   create_table "cars", force: :cascade do |t|
     t.integer  "seats"
