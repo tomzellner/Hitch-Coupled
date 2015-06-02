6878ee3ad9b4a9930c8c5e6d94fbfc41000f3a06
Shriyan Gunasekera <shriyan.gunasekera@gmail.com>
2015-06-01 17:49:59 -0700

    got text area to empty after submit

--- Hitch-Coupled/app/assets/javascripts/conversations.js
+++ Hitch-Coupled/app/assets/javascripts/conversations.js
@@ -11,7 +11,8 @@ var addMessage = function(event){
 
 
       var data = $('#new_message').serialize();
-      var url = $(this).attr("action")
+      var url = $(this).attr("action");
+      $('textarea#message_text').val("");
 
       var request = $.ajax({
         url: url,
@@ -21,6 +22,7 @@ var addMessage = function(event){
       }).done(function(response){
         var source = $('#message-template').html();
         var template = Handlebars.compile(source);
+
         $('#message-container').append(template(response))
       })
 }
