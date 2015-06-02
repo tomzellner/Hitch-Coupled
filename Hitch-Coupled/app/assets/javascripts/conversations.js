47bfaade8b1c01b8b67eef1e7074d2db61bc9ada
Shriyan Gunasekera <shriyan.gunasekera@gmail.com>
2015-06-01 17:36:37 -0700

    ajax calls working for new message with handlebars

--- Hitch-Coupled/app/assets/javascripts/conversations.js
+++ Hitch-Coupled/app/assets/javascripts/conversations.js
@@ -1,10 +1,26 @@
 $(document).ready(function(){
-  $('#new_message').on('submit', function(event){
+    eventBindings();
+});
+
+var eventBindings = function(){
+  $('#new_message').on('submit', addMessage)
+};
+
+var addMessage = function(event){
       event.preventDefault();
+
+
       var data = $('#new_message').serialize();
       var url = $(this).attr("action")
-      debugger
+
       var request = $.ajax({
+        url: url,
+        type: 'post',
+        data: data,
+        dataType: 'json'
+      }).done(function(response){
+        var source = $('#message-template').html();
+        var template = Handlebars.compile(source);
+        $('#message-container').append(template(response))
       })
-  })
-});
+}
