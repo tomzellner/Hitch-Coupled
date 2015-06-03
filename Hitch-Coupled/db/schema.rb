3955a9c50dab7bbb218f6474d078050d829d1fed
Yon <yaguilar213@gmail.com>
2015-06-02 22:24:28 -0700

    added more css

--- Hitch-Coupled/db/schema.rb
+++ Hitch-Coupled/db/schema.rb
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20150602224732) do
     t.integer  "user_id"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
-    t.string   "car_pic"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
