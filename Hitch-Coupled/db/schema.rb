54da94dcf01c01a5a1ff2530a20f087994882dbd
Tom Zellner <tom.zellner@gmail.com>
2015-06-04 10:01:52 -0700

    some doep ass car fixes

--- Hitch-Coupled/db/schema.rb
+++ Hitch-Coupled/db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150602174952) do
     t.string   "type_of"
     t.integer  "year"
     t.integer  "user_id"
+    t.string   "car_pic"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
