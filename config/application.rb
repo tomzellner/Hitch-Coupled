c6eaf888a544415eb640bc4f6f5df823f8f0e1e8
Tom Zellner <tom.zellner@gmail.com>
2015-06-10 22:47:00 -0700

    trying something else

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,12 @@ Bundler.require(*Rails.groups)
 
 module HitchCoupled
   class Application < Rails::Application
+
+
+
+    config.assets.precompile += [
+      'responsive.gs.12col.css'
+    ]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
